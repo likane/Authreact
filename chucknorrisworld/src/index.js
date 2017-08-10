4d0b82b16f62ae7747d574db94101dc242ea83ef
louiskane <lkane02@yahoo.com>
2017-08-09 18:51:11 -0700

    rewriting landing page

--- chucknorrisworld/src/index.js
+++ chucknorrisworld/src/index.js
@@ -1,8 +1,28 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+import { Router, Route, browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={FoodJokes}/>
+        <Route path="/special" component={CelebrityJokes}/>
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
